e3dd4898bf430a7aea7628dc140a155e5b5f83d7
nushadhb <77740447+nushadhb@users.noreply.github.com>
2022-10-27 14:11:23 -0400

    Update streamlit_app.py
    few lines ,text messages added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 
 import streamlit as st
-st.title('Streamlit learning')
-st.header('Building streamlit app')
-st.write("Hello this is my first streamlit application!")
+st.title('My Parents New Healthy Dinner')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothies')
+st.text('Hard-Boiled Free-Range Egg')
