f95440c5e8c6c0c44d5c883edc93fecf80c4e83d
nushadhb <77740447+nushadhb@users.noreply.github.com>
2022-10-27 14:36:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.text('🥑🍞 Avocodo Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #step2 learning.. , import pandas library and read csv file.
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 #step3 learning
 st.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocodo','Strawberries'])
 st.dataframe(my_fruit_list)
