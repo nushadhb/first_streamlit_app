37440e4c0b3fcb5de209f767acd5e61ec4c47d48
nushadhb <77740447+nushadhb@users.noreply.github.com>
2022-10-27 14:01:19 -0400

    Update streamlit_app.py
    code fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@
 import streamlit as st
 st.title('Stream list learning')
 st.header('Building streamlit app')
-st.write("Hello this is my first streamlit application!"
+st.write("Hello this is my first streamlit application!")
