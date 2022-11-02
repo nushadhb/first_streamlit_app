82f150c4ab02edad5a811b157f7b4d46370cc4e8
nushadhb <77740447+nushadhb@users.noreply.github.com>
2022-11-01 23:53:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ st.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from pc_rivery_db.public.FRUIT_LOAD_LIST")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_cur.execute("select * from pc_rivery_db.public.FRUIT_LOAD_LIST")
 
 my_data_row = my_cur.fetchone()
 st.text("Hello from Snowflake:")
