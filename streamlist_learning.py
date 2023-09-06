c5100d4452e693cf00764e5730f3a33cbd87cbc3
nushadhb <77740447+nushadhb@users.noreply.github.com>
2023-09-06 17:30:19 -0400

    Update streamlist_learning.py

--- streamlist_learning.py
+++ streamlist_learning.py
@@ -1,2 +1,11 @@
-import streamlist as st
+import streamlit as st
 import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
